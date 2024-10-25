f68a7a7c7b6b9f08be7b555a6d73b737d2dadbd5
Neider Nieto <nestnidev17@gmail.com>
2024-10-25 10:01:54 -0500

    add output to astro.config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
 
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel()
-});
+  adapter: vercel(),
+  output: 'server'
+})
