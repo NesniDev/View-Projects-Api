ce7fa7575ddc60cab7bf8df6b94507126e7faf8e
Neider Nieto <nestnidev17@gmail.com>
2024-10-25 12:58:30 -0500

    change adapter to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
 
-import cloudflare from '@astrojs/cloudflare';
+import cloudflare from '@astrojs/cloudflare'
+
+import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare()
+  adapter: vercel()
 })
