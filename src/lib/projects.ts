20bab2d10252d8ff826b2516728f4292d3939881
Neider Nieto <nestnidev17@gmail.com>
2024-10-25 09:52:31 -0500

    JSON of the projects

---
+++ src/lib/projects.ts
@@ -0,0 +1,32 @@
+const PROJECT_APIS = [
+    {
+        img: '/Pokemon.png',
+        name: 'Pokemon API',
+        description: 'API para obtener información de cada uno de los Pokemon', 
+        github: 'https://github.com/NesniDev/Apis-Projects/tree/main/pokemon',
+        webUrl: 'https://pokemon-neider.netlify.app'
+    },
+    {
+        img: '/Ip.png',
+        name: 'DirecciónIp API',
+        description: 'API para obtener la dirección IP de un usuario en formato JSON',
+        github: 'https://github.com/NesniDev/Apis-Projects/tree/main/direccion_ip',
+        webUrl: 'https://direccion-ip.netlify.app'
+    },
+    {
+        img: '/Peliculas.png',
+        name: 'Películas API',
+        description: 'API para obtener información de cada una de las películas en tendencia',
+        github: 'https://github.com/NesniDev/Apis-Projects/tree/main/peliculas',
+        webUrl: 'https://peliculas-neider.netlify.app'
+    },
+    {
+        img: '/R&M.png',
+        name: 'Rick and Morty API',
+        description: 'API para obtener información de cada uno de los personajes de Rick and Morty',
+        github: 'https://github.com/NesniDev/Apis-Projects/tree/main/rickandmorty',
+        webUrl: 'https://rick-morty-neider.netlify.app'
+    }
+]
+
+export { PROJECT_APIS } 
