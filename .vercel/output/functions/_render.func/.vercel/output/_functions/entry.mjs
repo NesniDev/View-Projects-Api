50ce3977837fcd0c6cab54b7f95e3e55cf935237
Neider Nieto <nestnidev17@gmail.com>
2024-10-25 10:40:40 -0500

    fix astro config

--- .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -1,6 +1,6 @@
 import { renderers } from './renderers.mjs';
 import { c as createExports } from './chunks/entrypoint_Cukk_v6V.mjs';
-import { manifest } from './manifest_TRbMyL49.mjs';
+import { manifest } from './manifest_BifWaWx7.mjs';
 
 const serverIslandMap = new Map([
 ]);;
@@ -19,7 +19,7 @@ const _manifest = Object.assign(manifest, {
     middleware: () => import('./_noop-middleware.mjs')
 });
 const _args = {
-    "middlewareSecret": "1fa42543-6b6b-4ade-a197-3280b259e50c",
+    "middlewareSecret": "89b8b35c-640c-490e-90bf-91ee00551421",
     "skewProtection": false
 };
 const _exports = createExports(_manifest, _args);
--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import vercel from '@astrojs/vercel/serverless'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-
+  output: 'static',
   adapter: vercel()
 })
