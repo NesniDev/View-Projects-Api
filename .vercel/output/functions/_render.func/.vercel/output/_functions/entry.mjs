5a9aa0578e6d86ea07961954c989c9c272c3749b
Neider Nieto <nestnidev17@gmail.com>
2024-10-25 10:29:23 -0500

    add vercel to astro

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,28 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_Cukk_v6V.mjs';
+import { manifest } from './manifest_TRbMyL49.mjs';
+
+const serverIslandMap = new Map([
+]);;
+
+const _page0 = () => import('./pages/index.astro.mjs');
+const _page1 = () => import('./pages/_image.astro.mjs');
+const pageMap = new Map([
+    ["src/pages/index.astro", _page0],
+    ["node_modules/.pnpm/astro@5.0.0-beta.5_typescript@5.6.3/node_modules/astro/dist/assets/endpoint/generic.js", _page1]
+]);
+
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "1fa42543-6b6b-4ade-a197-3280b259e50c",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/pages/_image.astro.mjs
@@ -0,0 +1,2 @@
+export { a as page } from '../chunks/generic_iRzOHO0w.mjs';
+export { renderers } from '../renderers.mjs';
